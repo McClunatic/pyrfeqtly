a52023cdfc4c1b0417f5814611f2e359e0d91fd3
Brian McClune <bpmcclune@gmail.com>
2024-05-06 12:36:32 -0400

    Add content to demo QFileSystemModel and views

--- src/pyrfeqt/__init__.py
+++ src/pyrfeqt/__init__.py
@@ -1,8 +1,32 @@
 # SPDX-FileCopyrightText: 2024-present Brian McClune <bpmcclune@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+import os
 import random
+from typing import Optional, Union
 from PySide6 import QtCore, QtWidgets
+
+
+class FileProxyModel(QtCore.QSortFilterProxyModel):
+    """See:
+
+    https://stackoverflow.com/questions/53430989/pyside-qfilesystemmodel-display-show-root-item
+    """
+    def __init__(
+        self,
+        index: Union[QtCore.QModelIndex, QtCore.QPersistentModelIndex],
+        parent: Optional[QtCore.QObject] = None,
+    ) -> None:
+        self._rootIndex = index
+        super(FileProxyModel, self).__init__(parent)
+
+    def filterAcceptsRow(self, sourceRow, sourceParent):
+        sourceIndex = self.sourceModel().index(sourceRow, 0, sourceParent)
+        if self._rootIndex.parent() == sourceParent and \
+                self._rootIndex != sourceIndex:
+            return False
+        return super(FileProxyModel, self).filterAcceptsRow(
+            sourceRow, sourceParent)
 
 
 class MyWidget(QtWidgets.QWidget):
@@ -15,7 +39,27 @@ class MyWidget(QtWidgets.QWidget):
         self.text = QtWidgets.QLabel("Hello World",
                                      alignment=QtCore.Qt.AlignCenter)
 
+        current_dir = QtCore.QDir.currentPath()
+        parent_dir = os.path.dirname(current_dir)
+
+        self.model = QtWidgets.QFileSystemModel()
+        self.model.setRootPath(parent_dir)
+        self.splitter = QtWidgets.QSplitter()
+        self.tree = QtWidgets.QTreeView(self.splitter)
+        self.list = QtWidgets.QListView(self.splitter)
+        self.proxy = FileProxyModel(
+            QtCore.QPersistentModelIndex(
+                self.model.index(current_dir)))
+        self.proxy.setSourceModel(self.model)
+        self.tree.setModel(self.proxy)
+        self.tree.setRootIndex(
+            self.proxy.mapFromSource(self.model.index(parent_dir)))
+        self.list.setModel(self.proxy)
+        self.list.setRootIndex(
+            self.proxy.mapFromSource(self.model.index(parent_dir)))
+
         self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.splitter)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
 
