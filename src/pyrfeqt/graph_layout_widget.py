423d3f2d1a4e98f74099348112bdb092c78b7dcb
Brian McClune <bpmcclune@gmail.com>
2024-05-12 10:24:35 -0400

    Change graph axiis ranges to degrees

--- src/pyrfeqt/graph_layout_widget.py
+++ src/pyrfeqt/graph_layout_widget.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-import time
-
 import numpy as np
 import pyqtgraph as pg
 
@@ -13,8 +11,8 @@ class GraphicsLayoutWidget(pg.GraphicsLayoutWidget):
         super().__init__(parent=parent, title=title)
 
         self.signal_plots = [self.addPlot() for _ in range(3)]
-        t = np.linspace(0., 4 * np.pi, 720)
-        y = np.sin(t + np.pi * time.time() / 30.)
+        t = 1. + np.arange(720.)
+        y = np.sin(t * np.pi / 180.)
         self.signal_curves = [plot.plot(t, y) for plot in self.signal_plots]
 
         self.nextRow()
@@ -23,6 +21,6 @@ class GraphicsLayoutWidget(pg.GraphicsLayoutWidget):
         for plot in self.spectr_plots:
             m = np.empty((300, 720))
             for dt in range(300):
-                m[dt, :] = np.sin(t + np.pi * (time.time() + 0.1 * dt) / 30.)
+                m[dt, :] = np.sin((t + dt) * np.pi / 180.)
             self.spectr_images.append(pg.ImageItem(m, colorMap='viridis'))
             plot.addItem(self.spectr_images[-1])
