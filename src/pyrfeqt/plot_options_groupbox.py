1816297bb927728b34e6488fdcee175c00dabf59
Brian McClune <bpmcclune@gmail.com>
2024-05-12 10:05:30 -0400

    Change plot options to QFormLayout

--- src/pyrfeqt/plot_options_groupbox.py
+++ src/pyrfeqt/plot_options_groupbox.py
@@ -3,9 +3,59 @@
 # SPDX-License-Identifier: MIT
 """Defines the plot options groupbox class."""
 
-from typing import Optional
+from typing import List, Optional
 
 from PySide6 import QtCore, QtWidgets
+
+
+class QSpinBox(QtWidgets.QSpinBox):
+    def sizeHint(self):
+        return super().sizeHint() + QtCore.QSize(40, 0)
+
+
+class SpinBoxStack(QtWidgets.QWidget):
+    def __init__(
+        self,
+        minimum: int,
+        maximum: int,
+        rows: int = 3,
+        parent: Optional[QtWidgets.QWidget] = None,
+    ) -> None:
+        """Constructor."""
+        super().__init__(parent=parent)
+
+        layout = QtWidgets.QGridLayout()
+        self.spins = []
+        for row in range(rows):
+            spinRow = []
+            for col in range(2):
+                spin = QSpinBox()
+                spin.setRange(minimum, maximum)
+                spin.setValue(maximum)
+                # spin.adjustSize()
+                layout.addWidget(spin, row, col)
+
+                spinRow.append(spin)
+            self.spins.append(spinRow)
+
+        self.setLayout(layout)
+
+
+class MultiModeBox(QtWidgets.QWidget):
+    def __init__(
+        self,
+        options: List[str],
+        parent: Optional[QtWidgets.QWidget] = None,
+    ) -> None:
+        """Constructor."""
+        super().__init__(parent=parent)
+
+        layout = QtWidgets.QVBoxLayout()
+        self.radios = [QtWidgets.QRadioButton(self.tr(opt)) for opt in options]
+        self.radios[0].setChecked(True)
+        for radio in self.radios:
+            layout.addWidget(radio)
+        self.setLayout(layout)
 
 
 class PlotOptionsGroupBox(QtWidgets.QGroupBox):
@@ -17,65 +67,13 @@ class PlotOptionsGroupBox(QtWidgets.QGroupBox):
         """Constructor."""
         super(PlotOptionsGroupBox, self).__init__(title=title, parent=parent)
 
-        self.maxi = 720
-        self.ranges = [[0, self.maxi]] * 3
-        self.multimodes = ['Multi-line', 'Sum', 'Average']
-        self.sliders = []
-        self.spinboxes = []
-        self.radios = []
+        layout = QtWidgets.QFormLayout()
 
-        # Create QSlider and QSpinBox widgets
-        for idx, (mini, maxi) in enumerate(self.ranges):
-            sliders = []
-            spinboxes = []
+        self.axisRanges = SpinBoxStack(0, 720, parent=self)
+        self.aggregationMode = MultiModeBox(
+            ['none (multi-line)', 'sum', 'average'], self)
 
-            loBound = self.maxi * idx // 3
-            upBound = self.maxi * (idx + 1) // 3
-            for bound in (loBound, upBound):
-                slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
-                slider.setFocusPolicy(QtCore.Qt.FocusPolicy.StrongFocus)
-                slider.setSingleStep(1)
-                slider.setMinimum(mini)
-                slider.setMaximum(maxi)
-                slider.setValue(bound)
+        layout.addRow('Axis ranges', self.axisRanges)
+        layout.addRow('Aggregation mode', self.aggregationMode)
 
-                spinbox = QtWidgets.QSpinBox()
-                spinbox.setSingleStep(1)
-                spinbox.setMinimum(mini)
-                spinbox.setMaximum(maxi)
-                spinbox.setValue(bound)
-
-                slider.sliderMoved.connect(spinbox.setValue)
-                spinbox.valueChanged.connect(slider.setValue)
-
-                sliders.append(slider)
-                spinboxes.append(spinbox)
-
-            self.sliders.append(sliders)
-            self.spinboxes.append(spinboxes)
-
-        # Create multi-source radio button options
-        radioWidget = QtWidgets.QWidget()
-        radioLayout = QtWidgets.QHBoxLayout()
-        for mode in self.multimodes:
-            radio = QtWidgets.QRadioButton(self.tr(mode))
-            self.radios.append(radio)
-
-            radioLayout.addWidget(radio)
-
-        self.radios[0].setChecked(True)
-        radioWidget.setLayout(radioLayout)
-
-        # Create layout
-        layout = QtWidgets.QGridLayout()
-        layout.setColumnMinimumWidth(3, 8)
-        layout.setColumnMinimumWidth(1, 80)
-        layout.setColumnMinimumWidth(4, 80)
-        for row in range(3):
-            layout.addWidget(self.sliders[row][0], row, 0)
-            layout.addWidget(self.spinboxes[row][0], row, 1)
-            layout.addWidget(self.sliders[row][1], row, 3)
-            layout.addWidget(self.spinboxes[row][1], row, 4)
-
-        layout.addWidget(radioWidget, 3, 0, 1, 5)
         self.setLayout(layout)
