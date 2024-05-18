1237e9f276ec9f9ce27d9a9b5732dacb1b966e46
Brian McClune <bpmcclune@gmail.com>
2024-05-18 09:48:08 -0400

    Update gen_samples.py to write two sources

--- tests/gen_samples.py
+++ tests/gen_samples.py
@@ -1,26 +1,40 @@
 import datetime
 import pathlib
-import sys
 import time
 
 import numpy as np
 
 
 def main():
-    dir_name = 'samples'
-    if len(sys.argv) > 1:
-        dir_name = sys.argv[1]
-    sample_dir = pathlib.Path(__file__).parent / dir_name
-    sample_dir.mkdir(exist_ok=True)
+    dirs = ['sine', 'cosine']
+    sample_dirs = []
+    for dir_name in dirs:
+        sample_dir = pathlib.Path(__file__).parent / dir_name
+        sample_dir.mkdir(exist_ok=True)
+        sample_dirs.append(sample_dir)
+
+    now = time.time()
     t = np.arange(720.)
     try:
         while True:
-            stamp = datetime.datetime.now().strftime('%Y.%m.%d.%H.%M.%S.%f')
-            sample_npy = sample_dir / f'{stamp}.npy'
-            with open(sample_npy, 'wb') as npy_file:
-                np.save(npy_file,
-                        np.sin((t + 90. * time.time()) * np.pi / 180.))
-            time.sleep(0.1)
+            dt = time.time() - now
+            for sample_dir in sample_dirs:
+                if sample_dir.name == 'sine':
+                    func = np.sin
+                    freq = 0.5
+                    ampl = 2
+                else:
+                    func = np.cos
+                    freq = 0.3
+                    ampl = 1
+                stamp = datetime.datetime.now().strftime(
+                    '%Y.%m.%d.%H.%M.%S.%f')
+                sample_npy = sample_dir / f'{stamp}.npy'
+                with open(sample_npy, 'wb') as npy_file:
+                    a = ampl * func(freq * (dt + t * np.pi / 180.))
+                    np.save(npy_file, a)
+                time.sleep(0.1)
+            time.sleep(0.8)
     except KeyboardInterrupt:
         return 0
     except Exception:
