acc06dbe3a1ab82a9ad6b42fa4c9b20cd335a6e8
Brian McClune <bpmcclune@gmail.com>
2024-05-19 10:30:41 -0400

    Ensure tracebacks are printed from slots

--- src/pyrfeqt/__main__.py
+++ src/pyrfeqt/__main__.py
@@ -2,8 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 import sys
+import traceback
 from PySide6 import QtWidgets
 from pyrfeqt import MainWindow
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    traceback.print_exception(exc_type, exc_value, exc_tb)
+
+
+sys.excepthook = excepthook
 
 app = QtWidgets.QApplication([])
 
